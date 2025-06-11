e450279d7f64b3921e8eec211818ba85b28575b4
GabrielKirch <gabriel.kirch@escola.pr.gov.br>
2025-06-11 09:07:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
 console.log(botoes);
-const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
